141da75f4c647f99db2fd001611c5ef199b37dc2
8.1 <marshachikita16@gmail.com>
2018-08-25 21:05:51 +0700

    Tampilan Akun Edit(1)

--- app/src/main/java/me/cekpedia/ProfileFragment.java
+++ app/src/main/java/me/cekpedia/ProfileFragment.java
@@ -3,11 +3,18 @@ package me.cekpedia;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -27,8 +34,14 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        setHasOptionsMenu(true);
+
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.my_toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
 
         //Pengaturan Font
         st = (TextView) view.findViewById(R.id.toolbar_text);
@@ -38,4 +51,27 @@ public class ProfileFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.option_menu, menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_bantuan:
+                Toast.makeText(getActivity(), "Action bantuan clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_keluar:
+                Toast.makeText(getActivity(), "Action bantuan clicked", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return false;
+    }
 }
