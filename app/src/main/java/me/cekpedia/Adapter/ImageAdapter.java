f8dc3abe57bf59b59e64238ba9189ed14346cbec
Reza Dwi Hendarno <rezadwihendarno@cekpedia.com>
2018-08-27 11:51:51 +0700

    folder nya udah dipisahin tapi login nya belum dibenerin

---
+++ app/src/main/java/me/cekpedia/Adapter/ImageAdapter.java
@@ -0,0 +1,78 @@
+package me.cekpedia.Adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import me.cekpedia.R;
+
+/**
+ * Created by rezadwihendarno on 18/02/2018.
+ */
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.MyHolder> {
+
+    private int icons[];
+    private String text[];
+    private Context context;
+    private ClickListener clickListener;
+
+    private LayoutInflater inflater;
+    View view;
+
+    public ImageAdapter(Context context,  int[] icons, String[] text) {
+        this.icons = icons;
+        this.text = text;
+        this.context = context;
+    }
+
+    @Override
+    public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_view, null);
+        MyHolder myHolder = new MyHolder(layout);
+        return myHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyHolder holder, int position) {
+        holder.imgview.setImageResource(icons[position]);
+        holder.txt.setText(text[position]);
+    }
+
+    public void setClickListener(ClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
+
+    @Override
+    public int getItemCount() {
+        return text.length;
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView imgview;
+        TextView txt;
+        public MyHolder(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+            imgview = itemView.findViewById(R.id.image_grid);
+            txt = itemView.findViewById(R.id.text_grid);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (clickListener != null){
+                clickListener.itemClicked(view, getPosition());
+            }
+        }
+    }
+
+    public interface ClickListener {
+        void itemClicked(View view, int position);
+
+    }
+
+}
---
+++ app/src/main/java/me/cekpedia/Adapter/ListCardAdapter.java
@@ -0,0 +1,98 @@
+package me.cekpedia.Adapter;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import me.cekpedia.R;
+
+public class ListCardAdapter extends RecyclerView.Adapter<ListCardAdapter.ViewHolder> {
+
+    private String[] nama = {"Nama Tempat 1",
+            "Nama Tempat 2",
+            "Nama Tempat 3",
+            "Nama Tempat 4",
+            "Nama Tempat 5",
+            "Nama Tempat 6",
+            "Nama Tempat 7",
+            "Nama Tempat 8"};
+
+    private String[] jarak = {"Jarak Tempat 1",
+            "Jarak Tempat 2",
+            "Jarak Tempat 3",
+            "Jarak Tempat 4",
+            "Jarak Tempat 5",
+            "Jarak Tempat 6",
+            "Jarak Tempat 7",
+            "Jarak Tempat 8"};
+
+    private String[] detail = {"Alamat lengkap tempat 1",
+            "Alamat lengkap tempat 2", "Alamat lengkap tempat 3",
+            "Alamat lengkap tempat 4", "Alamat lengkap tempat 5",
+            "Alamat lengkap tempat 6", "Alamat lengkap tempat 7",
+            "Alamat lengkap tempat 8"};
+
+    private int[] gambar = { R.drawable.hotelgrandia,
+            R.drawable.hotelhorison,
+            R.drawable.hotelibis,
+            R.drawable.apotekk24gerlong,
+            R.drawable.bidandelima,
+            R.drawable.bidanida,
+            R.drawable.masjidraya,
+            R.drawable.rshasansadikin };
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_nearme, parent, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        holder.itemNama.setText(nama[position]);
+        holder.itemDetail.setText(detail[position]);
+        holder.itemGambar.setImageResource(gambar[position]);
+        holder.itemJarak.setText(jarak[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return nama.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public int current;
+        public ImageView itemGambar;
+        public TextView itemNama, itemDetail, itemJarak;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            itemGambar = (ImageView)itemView.findViewById(R.id.gambar);
+            itemNama = (TextView)itemView.findViewById(R.id.namaTempat);
+            itemDetail = (TextView)itemView.findViewById(R.id.alamatTempat);
+            itemJarak = (TextView)itemView.findViewById(R.id.jarakTempat);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+                    Snackbar.make(v, "Click detected on item " + position,
+                            Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                }
+            });
+        }
+    }
+}
